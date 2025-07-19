ff2990c1c8fbb2a7384e3b2f5b5da08e4eaf7afe
rccnandakumar <rccnandakumar@gmail.com>
2025-07-19 19:27:48 +0530

    Update Cache Query

--- client/src/lib/graphql/hooks.js
+++ client/src/lib/graphql/hooks.js
@@ -1,12 +1,21 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { addMessageMutation, messagesQuery } from './queries';
+import { useMutation, useQuery } from "@apollo/client";
+import { addMessageMutation, messagesQuery } from "./queries";
+import { cache } from "react";
 
 export function useAddMessage() {
   const [mutate] = useMutation(addMessageMutation);
 
   const addMessage = async (text) => {
-    const { data: { message } } = await mutate({
+    const {
+      data: { message },
+    } = await mutate({
       variables: { text },
+      update: (cache, { data }) => {
+        const newMessage = data.message;
+        cache.updateQuery({ query: messagesQuery }, ({ messages }) => {
+          return { messages: [...messages, newMessage] };
+        });
+      },
     });
     return message;
   };
